20b87d416203d87924c68d4e1f271716e3ea11df
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-07-03 22:22:48 +0530

    setup angular http routes, create frontend services to engage with backend.

--- frontend/src/app/services/task.service.ts
+++ frontend/src/app/services/task.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../interfaces/task';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/tasks';
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  getTask(id: string): Observable<Task> {
+    return this.http.get<Task>(`${this.apiUrl}/${id}`);
+  }
+
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  updateTask(id: string, task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${id}`, task);
+  }
+
+  deleteTask(id: string): Observable<Task> {
+    return this.http.delete<Task>(`${this.apiUrl}/${id}`);
+  }
 }
